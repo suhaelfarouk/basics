a6c20520ff50e88a2b0d8b28185f74d0d1f7c062
Suhael Farouk <suhaelfarouk@gmail.com>
2022-09-15 10:09:48 +0530

    Input

---
+++ input.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    string name;
+    cout << "Enter your name: ";
+    getline(cin, name);
+    cout << "Hello " << name << endl;
+
+    int age;
+    cout << "Enter your age: ";
+    cin >> age;
+    cout << "You are " << age << " years old" << endl;
+
+    return 0;
+
+}
