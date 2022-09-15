46c54f69ca283ae9c7cffadad3cb905d431bb7c3
Suhael Farouk <suhaelfarouk@gmail.com>
2022-09-15 10:24:51 +0530

    Mad Libs

---
+++ madLibs.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    string color, pluralNoun, name;
+
+    cout << "Enter a color: ";
+    getline(cin, color);
+
+    cout << "Enter a plural noun: ";
+    getline(cin, pluralNoun);
+
+    cout << "Enter a name: ";
+    getline(cin, name);
+
+    cout << "Roses are " << color << endl;
+    cout << pluralNoun << " are blue" << endl;
+    cout << "I love " << name << endl;
+
+    return 0;
+}
