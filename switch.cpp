5402bbcdfa4450cb515e0a5f054460699243f05e
Suhael Farouk <suhaelfarouk@gmail.com>
2022-09-16 10:07:27 +0530

    Switch

---
+++ switch.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+
+string getDayOfWeek(int dayNum){
+    string dayName;
+
+    switch (dayNum){
+        case 0:
+            dayName = "Sunday";
+            break;
+
+        case 1:
+            dayName = "Monday";
+            break;
+
+        case 2:
+            dayName = "Tuesday";
+            break;
+
+        case 3:
+            dayName = "Wednesday";
+            break;
+        
+        case 4:
+            dayName = "Thursday";
+            break;
+        
+        case 5:
+            dayName = "Friday";
+            break;
+        
+        default:
+            dayName = "Invalid Day Numer";
+    }
+    return dayName;
+}
+
+int main()
+{
+    cout << getDayOfWeek(0) << endl;
+    return 0;
+}
